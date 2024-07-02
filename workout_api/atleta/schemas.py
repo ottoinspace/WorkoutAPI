7a9d869329073ba0f396f45542612b7d70452721
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2024-07-02 13:09:56 -0300

    Atleta controller complete

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,6 +1,8 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
 
+from workout_api.categorias.schemas import CategoriaIn
+from workout_api.centro_treinamento.schemas import CentroTreinamento
 from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 class Atleta(BaseSchema):
@@ -10,8 +12,14 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=80.6)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.8)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamento, Field(description='Centro de treinamento do atleta')]
 
 class AtletaIn(Atleta):
     pass
 class AtletaOut(Atleta, OutMixin):
     pass
+
+class AtletaUpdate(BaseSchema):
+     nome: Annotated[Optional[str], Field(description='Nome do atleta', example='Joao', max_length=50)]
+     idade: Annotated[Optional[int], Field(description='Idade do atleta', example=25)]
